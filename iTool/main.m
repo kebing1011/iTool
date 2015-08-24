ef2a7c31107f3d6c06c7b91ee832c2a6edf3aad1
maokebing <maokebing@aspirecn.com>
2015-08-24 09:35:18 +0800

    Initial Commit

---
+++ iTool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iTool
+//
+//  Created by Mao on 8/24/15.
+//  Copyright (c) 2015 Mao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
